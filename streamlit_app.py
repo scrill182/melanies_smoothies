ef399a897fbdcf44ea3336bd5506ebd02c262ff7
Carlos <scrill182@gmail.com>
2024-11-13 15:34:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,8 @@ if ingredients_list:
         
 ####################################################
        ### smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
-        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
-        sf_df= st.dataframe(data= smoothiefroot_response.json(),use_container_width=True)
+#        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
+#        sf_df= st.dataframe(data= smoothiefroot_response.json(),use_container_width=True)
 
   #  st.write(ingredients_string)
     
