c9837746d9428efefca006301443230e6078a28c
Carlos <scrill182@gmail.com>
2024-11-13 18:59:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,12 @@
-
-
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom smoothie!
-    """
-)
+    """)
 title = st.text_input("Name on Smoothie", "")
 st.write("The name on your smoothie is ", title)
 ####
@@ -29,7 +25,6 @@ ingredients_list = st.multiselect(
     , my_dataframe
     ,max_selections=5
 )
-
 if ingredients_list:
     ingredients_string= ''
 
@@ -37,7 +32,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen+' '
 ################################
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        ####st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen+ 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ search_on)
